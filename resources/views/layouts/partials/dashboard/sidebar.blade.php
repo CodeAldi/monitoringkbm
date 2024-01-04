5c4b11f9d4eae213db5b39c5414839e2448d71cf
aldi <codealdi@gmail.com>
2024-01-04 12:25:55 +0700

    remove sneat logo

--- resources/views/layouts/partials/dashboard/sidebar.blade.php
+++ resources/views/layouts/partials/dashboard/sidebar.blade.php
@@ -2,7 +2,7 @@
     <div class="app-brand demo">
         <a href="#" class="app-brand-link">
             <span class="app-brand-logo demo">
-                <svg width="25" viewBox="0 0 25 42" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                {{-- <svg width="25" viewBox="0 0 25 42" version="1.1" xmlns="http://www.w3.org/2000/svg"
                     xmlns:xlink="http://www.w3.org/1999/xlink">
                     <defs>
                         <path
@@ -43,9 +43,9 @@
                             </g>
                         </g>
                     </g>
-                </svg>
+                </svg> --}}
             </span>
-            <span class="app-brand-text demo menu-text fw-bolder ms-2">Sneat</span>
+            <span class="app-brand-text demo menu-text fw-bolder ms-2">Monitoring KBM</span>
         </a>
 
         <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
